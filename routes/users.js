3083e3a1bfc0a822ce9e20841c440eb5d78b549d
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-10 17:23:30 +0530

    users.js setup ✅

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/secondDB");
 
 const userSchema = mongoose.Schema({
   username: String,
-  fullName: String,
-  description: String,
-  categories: {
-    type: Array,
-    default: [],
-  },
-  dateCreated: {
-    type: Date,
-    default: Date.now,
-  },
+  password: String,
+  secret: String,
+  // fullName: String,
+  // description: String,
+  // categories: {
+  //   type: Array,
+  //   default: [],
+  // },
+  // dateCreated: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
 });
+
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("user", userSchema);
